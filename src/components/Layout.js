07fa8888ec7fa95a658efb9450a72d442fd48a75
Allan MacGregor <amacgregor@allanmacgregor.com>
2020-03-23 21:46:12 -0400

    Quick Metadata fix

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -45,7 +45,7 @@ const TemplateWrapper = ({ children }) => {
         <meta property="og:url" content="/" />
         <meta
           property="og:image"
-          content={`${withPrefix('/')}img/Offline2On-Logo-FC.svg`}
+          content={`${withPrefix('/')}img/og-image.jpg`}
         />
       </Helmet>
       <Navbar />
