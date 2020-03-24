03e451d05956482d1b66bbc2b0a1d5fd22d06d3d
Alex Luong <alex@alexluong.com>
2020-03-23 19:19:55 -0700

    meta img from relative to absolute url

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,10 +4,13 @@ import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 import './all.sass';
 import useSiteMetadata from './SiteMetadata';
-import { withPrefix } from 'gatsby';
+import image from '../../static/img/og-image.jpg';
+
+const URL = 'https://offline2on.com/';
 
 const TemplateWrapper = ({ children }) => {
   const { title, description } = useSiteMetadata();
+
   return (
     <div>
       <Helmet>
@@ -18,36 +21,39 @@ const TemplateWrapper = ({ children }) => {
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href={`${withPrefix('/')}img/apple-touch-icon.png`}
+          href={`${URL}img/apple-touch-icon.png`}
         />
         <link
           rel="icon"
           type="image/png"
-          href={`${withPrefix('/')}img/favicon-32x32.png`}
+          href={`${URL}img/favicon-32x32.png`}
           sizes="32x32"
         />
         <link
           rel="icon"
           type="image/png"
-          href={`${withPrefix('/')}img/favicon-16x16.png`}
+          href={`${URL}img/favicon-16x16.png`}
           sizes="16x16"
         />
 
         <link
           rel="mask-icon"
-          href={`${withPrefix('/')}img/safari-pinned-tab.svg`}
+          href={`${URL}img/safari-pinned-tab.svg`}
           color="#ff4400"
         />
         <meta name="theme-color" content="#fff" />
-        <meta name="twitter:card" content="summary_large_image"/>
-        <meta name="twitter:image" content={`${withPrefix('/')}img/og-image.jpg`} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content={`${URL}img/og-image.jpg`} />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={title} />
+        {/* <meta name="twitter:image" content={`${URL}img/og-image.jpg`} /> */}
+        <meta name="twitter:image" content={image} />
+
         <meta property="og:type" content="business.business" />
         <meta property="og:title" content={title} />
-        <meta property="og:url" content="/" />
-        <meta
-          property="og:image"
-          content={`${withPrefix('/')}img/og-image.jpg`}
-        />
+        <meta property="og:url" content={URL} />
+        <meta property="og:image" content={`${URL}img/og-image.jpg`} />
       </Helmet>
       <Navbar />
       <div>{children}</div>
