0d8f2fae1b335f31b761a97c8db3848be5a258ad
Allan MacGregor <amacgregor@allanmacgregor.com>
2020-04-01 13:46:42 -0400

    Home video modal (#61)
    * Added Video Modal to homepage
    
    * fixed 'how it works' icons
    
    Co-authored-by: Luke Sedmak <luke@lukesedmak.com>

---
+++ src/components/HomePage/ModalVideo.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ModalVideo from 'react-modal-video'
+
+class App extends React.Component {
+
+  constructor () {
+    super()
+    this.state = {
+      isOpen: false
+    }
+    this.openModal = this.openModal.bind(this)
+  }
+
+  openModal () {
+    this.setState({isOpen: true})
+  }
+
+  render () {
+    return (
+      <div>
+        <ModalVideo channel='youtube' isOpen={this.state.isOpen} videoId='L61p2uyiMSo' onClose={() => this.setState({isOpen: false})} />
+        <button onClick={this.openModal}>Open</button>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+    document.getElementById('root')
+)
